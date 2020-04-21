e8c1dbc920d8348b82c73b3961d346183aefc626
Paolo Tesser <p.tesser921@gmail.com>
2020-04-21 23:18:44 +0200

    feat(ng-wordpres-api): add Wordpress models

--- projects/ng-wordpress-api/src/public-api.ts
+++ projects/ng-wordpress-api/src/public-api.ts
@@ -1,6 +1,6 @@
 /*
  * Public API Surface of ng-wordpress-api
  */
-
+export * from './lib/models';
 export * from './lib/services/ng-wordpress-api.service';
 export * from './lib/ng-wordpress-api.module';

28e2bae00cfaf4eed2bc572ac3667f8c6c9b4d67
Paolo Tesser <p.tesser921@gmail.com>
2020-04-29 22:33:39 +0200

    feat(ng-wordpress-api): bar module for services

--- projects/ng-wordpress-api/src/public-api.ts
+++ projects/ng-wordpress-api/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ng-wordpress-api
  */
 export * from './lib/models';
+export * from './lib/services';
 export * from './lib/services/ng-wordpress-api.service';
 export * from './lib/ng-wordpress-api.module';
